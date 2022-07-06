fd8d00a9b8a4f6a5f343fddd0268f36922e7248d
Glebson Santos <glebsonsantos@outlook.pt>
2022-07-06 10:42:59 -0300

    Entidades Curso e Categoria Criadas no BackEnd

---
+++ Api/CursosCast/Cursos.Api/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cursos.Api.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        [StringLength(50)]
+        public string CategoriaName { get; set; }
+        public ICollection<Curso> Cursos { get; set; }
+    }
+}
---
+++ Api/CursosCast/Cursos.Api/Models/Curso.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cursos.Api.Models
+{
+    public class Curso
+    {
+        public int CursoId { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFinal { get; set; }
+        public int QuantidadeAlunos { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
