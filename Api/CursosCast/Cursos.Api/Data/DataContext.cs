2115f6165537edc3ef7a44610ab4a893ab286456
Glebson Santos <glebsonsantos@outlook.pt>
2022-07-06 11:05:51 -0300

    Classe DataContext Criada, string de conexão tbm, alem do migration ja feito

---
+++ Api/CursosCast/Cursos.Api/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Cursos.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cursos.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Curso> Curso { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
---
+++ Api/CursosCast/Cursos.Api/Migrations/20220706135858_EntidadesCursoeCategoria.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cursos.Api.Migrations
+{
+    public partial class EntidadesCursoeCategoria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    CategoriaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoriaName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.CategoriaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Curso",
+                columns: table => new
+                {
+                    CursoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataInicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataFinal = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    QuantidadeAlunos = table.Column<int>(type: "int", nullable: false),
+                    CategoriaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Curso", x => x.CursoId);
+                    table.ForeignKey(
+                        name: "FK_Curso_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "CategoriaId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Curso_CategoriaId",
+                table: "Curso",
+                column: "CategoriaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Curso");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
