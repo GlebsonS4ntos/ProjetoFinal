7903dfea516603703ecab1f48dea2e2ec1548f4c
Glebson Santos <glebsonsantos@outlook.pt>
2022-07-09 22:13:11 -0300

    Refazendo navbar e adicionando a home page

--- Front/Cursos/src/app/Components/navbar/navbar.component.ts
+++ Front/Cursos/src/app/Components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-navbar',
@@ -8,7 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
   logo = "assets/logo/logo.png";
 
-  constructor() { }
+  constructor() {
+    $(function(){
+      $('ul li a').click(function(i){
+        $('ul li a').removeClass('active');
+        $(this).addClass('active');
+      });
+      $('ul li a').hover(function(i){
+        $('ul li a').removeClass('active');
+        $(this).addClass('active');
+      });
+    });
+  }
 
   ngOnInit(): void {
   }
